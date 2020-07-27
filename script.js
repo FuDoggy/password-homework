574f6e1582ac1b7950a6925373f95431e89c3910
Kevin Fujimoto <fujimoto.kevin@gmail.com>
2020-07-27 15:43:11 -0700

    practically done, small changes needed

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 
 var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var specialCharsArray = ['!', '"', '#', '$', '%', '&', '(', ')', '*', ',', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', "`", '{', '|', '}', '~'];
+var specialCharsArray = ['!', '"', '#', '$', '%', '&', '(', ')', '*', ',', '+', ',', '-', '.', '/', '\\',':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', "`", '{', '|', '}', '~'];
 var numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var newArray = [];
 var finalArray = [];
 
-var userPasswordChoiceLength = prompt ("How many characters would you like your password to be?");
 
-  userPasswordChoiceLength = parseInt (userPasswordChoiceLength);
-  console.log (userPasswordChoiceLength)
-  
-    if (userPasswordChoiceLength < 8 || userPasswordChoiceLength > 128) {
-      alert ("The character amount must be between 8 and 128 characters!");
-    } 
+function generatePassword(){
+
+  var lengthOK = false;
+  var userPasswordChoiceLength;
+
+  while(!lengthOK){
+    userPasswordChoiceLength = +prompt("How many characters would you like your password to be?")
     if (isNaN(userPasswordChoiceLength)) {
-      alert ("Password length must be provided as a number!")
+      alert ("Password length must be provided as a number!");
+    }else if(userPasswordChoiceLength < 8 || userPasswordChoiceLength > 128){
+      alert ("The character amount must be between 8 and 128 characters!")
+  }else{
+      lengthOK = true;
     }
+  }
+    
   
   var userPasswordUpperChoice = confirm ("Would you like uppercase letters in your password?");
     if (userPasswordUpperChoice) {
@@ -46,8 +52,10 @@ var userPasswordChoiceLength = prompt ("How many characters would you like your 
       for (var i = 0; i <userPasswordChoiceLength; i++) {
         finalArray = finalArray + newArray [Math.floor(Math.random() * newArray.length)]
       }
-      console.log(finalArray)
-
+      console.log(finalArray);
+      return finalArray
+    }
+  
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -55,10 +63,10 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
+
+var person;
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
