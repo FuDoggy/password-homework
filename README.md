3d15cd2259023602807751199ed0a8397df8f56d
Kevin Fujimoto <fujimoto.kevin@gmail.com>
2020-07-27 21:37:40 -0700

    final pass with enhanced error handling and comments

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+<<ALERT>>  I WORKED WITH TIM HELLMAN ON THIS HOMEWORK, SO OUR CODE WILL LOOK SIMILAR.
+
 # 03 JavaScript: Password Generator
 
 Create an application that generates a random password based on user-selected criteria. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+//Declaring arrays for uppercase letters, lowercase letters, special characters, and numbers.  
+//Also declaring empty arrays for newArray (for manipulation) and finalArray to pass in as the final password to the user.
 var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var specialCharsArray = ['!', '"', '#', '$', '%', '&', '(', ')', '*', ',', '+', ',', '-', '.', '/', '\\',':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', "`", '{', '|', '}', '~'];
@@ -6,54 +7,62 @@ var numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var newArray = [];
 var finalArray = [];
 
-
+//Call function generatePassword()
 function generatePassword(){
 
+//Declare and assign user character length variables for evaluation and error handling purposes
   var lengthOK = false;
   var userPasswordChoiceLength;
 
-  while(!lengthOK){
-    userPasswordChoiceLength = +prompt("How many characters would you like your password to be?")
+  //while loop to execute error handling and make sure the user inputs a number, and a number that is between 8 and 128 characters
+  while(!lengthOK) {
+    userPasswordChoiceLength = +prompt("How many characters would you like your password to be?");
     if (isNaN(userPasswordChoiceLength)) {
       alert ("Password length must be provided as a number!");
-    }else if(userPasswordChoiceLength < 8 || userPasswordChoiceLength > 128){
+    }
+    else if(userPasswordChoiceLength < 8 || userPasswordChoiceLength > 128){
       alert ("The character amount must be between 8 and 128 characters!")
-  }else{
+    }
+    else {
       lengthOK = true;
     }
   }
     
-  
+  //Confirm with the user that they want uppercase letters in their password, and if so, add it to the array of possible characters
   var userPasswordUpperChoice = confirm ("Would you like uppercase letters in your password?");
     if (userPasswordUpperChoice) {
       newArray = newArray.concat(upperCaseArray);
       }
       console.log(newArray);
 
+  //Confirm with the user that they want lowercase letters in their password, and if so, add it to the array of possible characters
   var userPasswordLowerChoice = confirm ("Would you like lowercase letters in your password?");
     if (userPasswordLowerChoice) {
       newArray = newArray.concat(lowerCaseArray);
       }
       console.log(newArray);
 
+  //Confirm with the user that they want special characters in their password, and if so, add it to the array of possible characters    
     var userPasswordSpecCharsChoice = confirm ("Would you like special characters in your password?");
-    if (userPasswordSpecCharsChoice) {
-      newArray = newArray.concat(specialCharsArray);
+      if (userPasswordSpecCharsChoice) {
+        newArray = newArray.concat(specialCharsArray);
       }
-      console.log(newArray)
+        console.log(newArray);
 
+  //Confirm with the user that they want numbers in their password, and if so, add it to the array of possible characters 
     var userPasswordNumbersChoice = confirm ("Would you like numbers in your password?");
-    if (userPasswordNumbersChoice) {
-      newArray = newArray.concat(numbersArray);
+      if (userPasswordNumbersChoice) {
+        newArray = newArray.concat(numbersArray);
       }
-      console.log(newArray)
+      console.log(newArray);
 
-    var finalArray = ""
+  //Cut down password length to user specs, randomize, and return final password 
+      var finalArray = ""
       for (var i = 0; i <userPasswordChoiceLength; i++) {
         finalArray = finalArray + newArray [Math.floor(Math.random() * newArray.length)]
       }
       console.log(finalArray);
-      return finalArray
+      return finalArray;
     }
   
 // Assignment Code
@@ -65,8 +74,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-
-var person;
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
